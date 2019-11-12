e2d21754b5bb70f70005c8fb79690bb971fecefd
Loren Wetzel <l.wetzel900@gmail.com>
2019-11-11 22:03:20 -0600

    week 4 files

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -25,3 +25,13 @@ var User = mongoose.model("User", userSchema);
 
 // make the userSchema accessible
 module.exports = User;
+
+//user.save is used to add a new user in database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = { _id: id };
+    User.findById(query, callback);
+};
