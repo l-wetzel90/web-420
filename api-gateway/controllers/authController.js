c8d6506a55f454be4c3cdd612780a0abb833ad22
Loren Wetzel <l.wetzel900@gmail.com>
2020-03-16 17:55:11 -0500

    fixing messups

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -1,74 +1,63 @@
-var User = require('../models/user');
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
-var config = require('../config');
+var User = require("../models/user");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+var config = require("../config");
 
 //register a new user on post
-exports.user_register = function (req, res) {
+exports.user_register = function(req, res) {
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  var newUser = new User({
+    username: req.body.username,
+    password: hashedPassword,
+    email: req.body.email
+  });
 
-    var newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-        email: req.body.email
+  User.add(newUser, (err, user) => {
+    if (err)
+      return res.status(500).send("There was a problem registering the user.");
+
+    var token = jwt.sign({ id: user._id }, config.web.secret, {
+      expiresIn: 86400 // 24 hours
     });
 
-    User.add(newUser, (err, user) => {
-        if (err)
-            return res.status(500).send('There was a problem registering the user.');
-
-        var token = jwt.sign({ id: user._id }, config.web.secret, {
-            expiresIn: 86400 // 24 hours
-        });
-
-        res.status(200).send({ auth: true, token: token });
-    });
+    res.status(200).send({ auth: true, token: token });
+  });
 };
 
 // Verify token on GET
-exports.user_token = function (req, res) {
+exports.user_token = function(req, res) {
+    
+  User.getById(req.userId, function(err, user) {
+    if (err)
+      return res.status(500).send("There was a problem finding the user.");
 
-    // var token = req.headers['x-access-token'];
+    if (!user) return res.status(404).send("No user found.");
 
-    // if (!token) return res.status(401).send({ auth: false, message: 'No token provided' });
-
-    // jwt.verify(token, config.web.secret, function (err, decoded) {
-    //     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-        User.getById(req.userId, function (err, user) {
-            if (err) return res.status(500).send('There was a problem finding the user.');
-
-            if (!user) return res.status(404).send('No user found.');
-
-            res.status(200).send(user);
-        });
-
-    // });
+    res.status(200).send(user);
+  });
 };
 
 // User login request
-exports.user_login = function (req, res) {
+exports.user_login = function(req, res) {
+  User.getOne(req.body.email, function(err, user) {
+    if (err) return res.status(500).send("Error on server.");
+    if (!user) return res.status(404).send("No user found.");
 
-    User.getOne(req.body.email, function (err, user) {
-        if (err) return res.status(500).send('Error on server.');
-        if (!user) return res.status(404).send('No user found.');
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
-        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid)
+      return res.status(401).send({ auth: false, token: null });
 
-        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-
-        var token = jwt.sign({ id: user._id }, config.web.secret, {
-            expiresIn: 86400 //expires in 24 hours
-        });
-
-        res.status(200).send({ auth: true, token: token });
+    var token = jwt.sign({ id: user._id }, config.web.secret, {
+      expiresIn: 86400 //expires in 24 hours
     });
+
+    res.status(200).send({ auth: true, token: token });
+  });
 };
 
 //User logout request
-exports.user_logout = function (req, res) {
-    res.status(200).send({ auth: false, token: null });
+exports.user_logout = function(req, res) {
+  res.status(200).send({ auth: false, token: null });
 };
-
-
